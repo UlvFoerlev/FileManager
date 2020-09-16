98e3c7502831461189d1b35c2101533c0ff35ab0
UlvFoerlev <thesilentparadox@gmail.com>
2020-09-16 17:16:26 +0200

    rules

--- src/rules.py
+++ src/rules.py
@@ -1,14 +1,51 @@
 
 #Actions
-# unzip_to - unzips the archive into target named folder
-# unzip - unzips an archive in the folder it is it
-# move - moves the file to a prenamed folder
-# delete - deletes the file
-# to_folder - creates a folder with the "target" as name, and put the file into it
+# unzip_to - unzips the archive into target named folder ✓
+# unzip - unzips an archive in the folder it is it ✓
+# move - moves the file to a prenamed folder ✓
+# delete - deletes the file ✓
+# rename ✓
 
-#targets
-# type - by file type
-# ext - by file extention
-# in_name - looks for substring in name
-# prefix - looks for substring at start of name
-# postfix - looks for substring at start of name
+#targets 
+# type - by file type ✓
+# ext - by file extention ✓
+# in_name - looks for substring in name ✓
+# prefix - looks for substring at start of name ✓
+# postfix - looks for substring at start of name ✓
+
+
+# TODO:
+#   And or targets
+
+
+rules = [{"target" : "type", "args" : "image", "action" : {"func" : "action_print"}}]
+
+class Rule:
+    def __init__(self):
+        pass
+
+
+class ExtensionType(Rule):
+    
+    def __eq__(self, other):
+        return other == "image"
+
+class Extension(Rule):
+    pass
+
+class Substring(Rule):
+    pass
+
+class Prefix(Rule):
+    pass
+
+class PostFix(Rule):
+    pass
+
+class Targets(list):
+    def where(self, eval):
+        print(eval)
+
+
+if __name__ == "__main__":
+    t = Targets().where(ExtensionType=="image")
